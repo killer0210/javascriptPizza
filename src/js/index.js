4b12256ef8c329dfaa3b44f8f6153fc579cb6139
killer0210 <132190909+killer0210@users.noreply.github.com>
2024-04-29 13:55:55 +0800

    First commit

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+require("babel-polyfill");
+import Search from "./model/search";
+
+let search = new Search("pizza");
+
+search.doSearch().then(r => console.log(r));
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+    mode: 'development',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+};
